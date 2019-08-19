95f985d3effc8f5665ab20ddce2b2c08d61302b7
Chen Weiguang <chen.weiguang@gmail.com>
2019-08-19 11:52:24 +0800

    Clean up script

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -7,6 +7,9 @@ c.LocalAuthenticator.create_system_users = True
 # Use JupyterLab by default
 c.Spawner.default_url = '/lab'
 
+# To prevent default kernel from appearing
+c.KernelSpecManager.ensure_native_kernel = False
+
 # These env vars will be retrieved at runtime
 # $OAUTH_CALLBACK_URL: http[s]://[your-host]/hub/oauth_callback
 # $OAUTH_CLIENT_ID
