2885d3643c6220e1e8f18b27183f7b4a97809b28
Chen Weiguang <chen.weiguang@gmail.com>
2019-08-20 10:05:00 +0800

    Install all via conda and use corrected jupyterhub version first

--- push-images.sh
+++ push-images.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker push "${DOCKER_IMAGE}:${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
+docker push "${DOCKER_IMAGE}:${JUPYTERHUB_VERSION}_spark-${SPARK_VERSION}_hadoop-${HADOOP_VERSION}"
